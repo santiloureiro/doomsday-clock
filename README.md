08cda1e5d92065c0063f8a1bb7a7c5de4bff9107
santiloureiro <santiloureiro12@gmail.com>
2025-07-21 23:48:44 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # doomsday-clock
+
+Small project that shows a clock that indicates when a target date comes when it reaches 'Se viene'.
---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import blood from "./assets/blood-splatter.png"
+import "./App.css";
+
+function App() {
+  const [today, setToday] = useState(new Date());
+
+  const dateToCome = new Date("07/28/2025 09:00");
+
+  const getPercentageDiff = (a: number, b: number, increment: number) => {
+    return increment * Math.abs((a - b) / ((a + b) / 2));
+  };
+
+  const diffTime = Math.abs(dateToCome.valueOf() - today.valueOf());
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  const percentageRemaining = getPercentageDiff(
+    dateToCome.valueOf(),
+    today.valueOf(),
+    10000
+  );
+
+  const calculateHandPos = () => {
+    const start = -90;
+    const final = 270;
+
+   const diff =  ((final - start) / (final - start)) * 100
+    
+    const handPosition = 90 + (diff * percentageRemaining)
+    
+    console.log(handPosition)
+    
+    return(handPosition)
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      setToday(new Date());
+    }, 1000);
+  });
+
+  return (
+    <>
+    <p style={{fontSize: "32px"}}>Se viene el {dateToCome.toLocaleDateString()} {dateToCome.toLocaleTimeString()}:</p>
+      <section>
+        <div className="label-container"><span className="label"><img src={blood} className="blood-splatter" /><span className="label-text">SE VIENE</span></span></div>
+        {/* <div className="hourhand"></div>
+        <div className="secondhand"></div> */}
+        <div className="minutehand" style={{transform: `rotate(-${calculateHandPos()}deg)`}}></div>
+        <div className="hour12"></div>
+        <div className="hour1"></div>
+        <div className="hour2"></div>
+        <div className="hour3"></div>
+        <div className="hour4"></div>
+        <div className="hour5"></div>
+      </section>
+    </>
+  );
+}
+
+export default App;
